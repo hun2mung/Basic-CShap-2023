a50dfab1691ad62c02575fd3f695f542000819cf
hun2mung <ldh5444@naver.com>
2023-04-04 12:13:16 +0900

    c# 첫번째 커밋

---
+++ Day01/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+// 네임스페이스 ConsoleApp1
+namespace ConsoleApp1       // 프로그램 명과 클래스 이름을 최대한 맞춰라
+{
+    /// <summary>
+    /// 프로그램 클래스
+    /// </summary>
+    internal class Program      
+    {
+        /// <summary>
+        /// 메인 엔트리 포인트
+        /// </summary>
+        /// <param name="args">콘솔 매개변수</param>
+        static void Main(string[] args)     // 진입점, args : string 배열의 매개변수(arguments)
+        {
+            Console.WriteLine("Hello C#!!");     // 콘솔에 글자 출력하는 메소드는 System에 들어있다.
+        }
+    }
+}
